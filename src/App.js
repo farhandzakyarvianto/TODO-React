6b7d53ead3bb6dbebf641f1693c33582dbf5936e
farhandzakyarvianto <farhandzaky25@gmail.com>
2020-07-15 21:10:03 +0700

    disable button when input empty

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <h1>Hello World </h1>
       <form>
         <input value={input} onChange={event => setInput(event.target.value)} />
-        <Button type="submit" onClick={addTodo} variant="contained" color="primary" >Add Todo</Button>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary" >Add Todo</Button>
       </form>
       <ul>
         {todos.map(todo => (
