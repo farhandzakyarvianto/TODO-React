4102ec113ca62519576bf86714645b1708fca319
farhandzakyarvianto <farhandzaky25@gmail.com>
2020-07-15 21:07:33 +0700

    add material ui design on button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Button} from '@material-ui/core'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dog for a walk', 'Take the rubbish out', 'testing']);
+  const [input, setInput] = useState('');
+  console.log(input);
+
+  const addTodo = (event) => {
+    //fungsi yang menghandle saat button di click
+    event.preventDefault(); //mencegah refresh
+    console.log('Im working');
+    setTodos([...todos, input]);
+    setInput(''); //set input kosong setelah klik button
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-
-      <input/ >
-      <button>Add Todo</button>
+      <h1>Hello World </h1>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <Button type="submit" onClick={addTodo} variant="contained" color="primary" >Add Todo</Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
